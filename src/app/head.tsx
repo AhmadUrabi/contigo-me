dc3309eec647fb88f45e7582f14d51c6e63f6a04
Ahmad Urabi <hamod9041@gmail.com>
2023-02-25 19:09:44 +0300

    Google Analytics

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,6 +1,15 @@
 export default function Head() {
   return (
     <>
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-R8GD2D2JL6"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-R8GD2D2JL6');
+      </script>
       <title>Contigo Middle East</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Contigo Middle East" />
